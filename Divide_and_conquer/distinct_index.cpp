56ea162863053f5458929c40c621aaa9a8aa6d73
Saipadmesh <saipadmesh@gmail.com>
2021-08-17 09:04:35 +0530

    17-8-21 commit

--- Divide_and_conquer/distinct_index.cpp
+++ Divide_and_conquer/distinct_index.cpp
@@ -3,23 +3,17 @@
 #include <vector>
 using namespace std;
 
-void printArray(vector<int> arr) {
-  int n = arr.size();
-  for (int i = 0; i < n; i++) {
-    cout << arr[i] << " ";
-  }
-  cout << endl;
-}
-
 vector<int> divide(vector<int> &arr, int begin, int end) {
   if (begin > end) {
     return vector<int>{0, -1};
   }
 
   int middle = (end == begin) ? end : (end + begin) / 2;
-  // cout << "middle: " << middle << endl;
-  if (arr[begin] == begin) {
-    return vector<int>{1, middle};
+
+  if (arr[middle] == middle) {
+    vector<int> a = divide(arr, begin, middle - 1);
+
+    return (a[0] == 0) ? vector<int>{1, middle} : a;
   }
   vector<int> a = divide(arr, begin, middle - 1);
   vector<int> b = divide(arr, middle + 1, end);
@@ -27,19 +21,9 @@ vector<int> divide(vector<int> &arr, int begin, int end) {
     return a;
   } else if (b[0] == 1) {
     return b;
-  } else if (a[0] == 1 && b[0] == 1) {
-    return (a[1] < b[1]) ? a : b;
   } else {
     return vector<int>{0, -1};
   }
-
-  //   else if (arr[middle] > middle) {
-
-  //     return divide(arr, middle + 1, end);
-  //   }
-  //   else {
-  //     return divide(arr, begin, middle - 1);
-  //   }
 }
 
 int main() {
@@ -60,7 +44,7 @@ int main() {
       }
       j++;
     }
-    // printArray(nums);
+
     vector<int> ans = divide(nums, 0, j - 1);
     if (ans[0] == 0) {
       cout << "False " << ans[1] << endl;
